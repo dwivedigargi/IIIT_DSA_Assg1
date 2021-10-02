f0a6db0b12c3a0b71962860d0156a85b55bd39d5
Gargi Dwivedi <dwivedigargi@gmail.com>
2021-10-02 19:33:41 +0530

    k distinct + on insert replace implemented

--- unordered_map.cpp
+++ unordered_map.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include<string>
 #include<typeinfo>
-#define q 3
+#define q 101
 using namespace std;
 
 template <typename T1, typename T2>
@@ -47,8 +47,10 @@ void HTable<T1, T2> ::traverse() {
     Node<T1, T2> *temp = new Node<T1, T2>();
     for(int i=0; i<q; i++) {
         temp=arr[i];
+        if(arr[i]==nullptr)
+            continue;
         while(temp) {
-            cout<<temp->key<<" ";
+            cout<<temp->key<<": "<<temp->value<<" ";
             temp=temp->next;
         }
         cout<<endl;
@@ -56,8 +58,10 @@ void HTable<T1, T2> ::traverse() {
 }
 
 template <typename T1, typename T2>
-void HTable<T1, T2>::insert(T1 key, T2 value) {
-    int h=Hash<T1>(key);    //use to_string() if key is not string
+void HTable<T1, T2>::insert(T1 key, T2 value) { //replace if already exists.
+    if(find(key))
+        erase(key);
+    int h=Hash<T1>(key);
     Node<T1, T2> *temp = new Node<T1, T2>();
     temp->key=key;
     temp->value=value;
@@ -69,7 +73,6 @@ void HTable<T1, T2>::insert(T1 key, T2 value) {
         temp->next=arr[h];
         arr[h]=temp;
     }
-    cout << "Hash value :" << h << endl;
 }
 
 template <typename T1, typename T2>
@@ -129,33 +132,50 @@ T2 HTable<T1, T2>:: operator[] (T1 key) {
     }
 }
 
+template <typename T1>
+void countDistinct (T1 A[], int n, int k, int res[])
+    {
+        HTable<T1, int> count;
+        int distinct=0, i;
+        for(i=0; i<k; i++)
+            if(count.find(A[i])) {
+                count.insert(A[i], count[A[i]]+1);
+            }
+            else {
+                count.insert(A[i], 1);
+                distinct+=1;
+            }
+        res[0]=distinct;
+        int rem=0;
+        while(i<n) {
+            if(!count.find(A[i])) {
+                count.insert(A[i],1);
+                distinct++;
+            }
+            else {
+                count.insert(A[i], count[A[i]]+1);
+            }
+            if(count[A[rem]]==1) {
+                count.erase(A[rem]);
+                distinct--;
+            }
+            else {
+                count.insert(A[rem], count[A[rem]]-1);
+            }
+            i++;
+            rem++;
+            res[rem]=distinct;
+        }
+    res[++rem]=-1;
+}
+
 int main () {
-    int n;
-    double key;
-    HTable<double, double> h;
-    double value;
-    cin>>n;
-    //HTable<string, int> h;
-    //h.erase("gargi");
-    for(int i=0; i<n; i++) {
-//        cin.ignore();
-  //      getline(cin, key);
-        cin >> key;
-        cin>>value;
-        h.insert(key, value);
-    }
-    //h.erase("gargi");
-    //h.erase("gargi");
-    //cout<<h.find("gargi");
-    //h.insert("gargi", 2);
-    //cout<<h.find("gargi");
-    //h.erase("gargi");
-    //cout<<h.find("gargi");
-    h.traverse();
-    int k; cin >> k;
-    for(int i =0; i < k; i++){
-        double inp;
-        cin >> inp;
-        cout << h[inp] << endl;
+    string a[]={"gargi", "bhairavi", "gargi", "stuti", "angel", "gargi", "stuti"};
+    int ans[100];
+    countDistinct<string>(a, 7, 4, ans);
+    int i=0;
+    while(ans[i]!=-1) {
+        cout<<ans[i]<<" ";
+        i++;
     }
 }
